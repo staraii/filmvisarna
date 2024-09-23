8cda4263c9eca1321e8d67f39f7231e00fea027d
Staffan Björk <staffanbjork86@gmail.com>
2024-09-23 09:18:56 +0200

    ui and styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "../sass/main.scss";
 import App from './App.tsx'
-import './index.css'
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
