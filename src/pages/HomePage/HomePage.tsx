e7d52619360d481222bb48e2f1cbd38a0f91518c
Staffan Björk <staffanbjork86@gmail.com>
2024-09-23 21:27:20 +0200

    HomePage UI created

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
+import MobileNavBar from "../../components/MobileNavBar/MobileNavBar";
+import WideNavBar from "../../components/WideNavBar/WideNavBar";
 import "./home-page.css";
-//import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import DropdownButton from "react-bootstrap/DropdownButton"
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import Card from "react-bootstrap/Card";
-//import Offcanvas from "react-bootstrap/Offcanvas";
+import Button from "react-bootstrap/Button";
+
+import Stack from "react-bootstrap/Stack";
 
 const visningar = [
   {
@@ -24,6 +25,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/titanic_poster.jpg",
+    status: "Low"
   },
   {
     title: "Sleepers",
@@ -33,6 +35,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/sleepers_poster.jpg",
+    status: "Medium"
   },
   {
     title: "Pippi",
@@ -42,6 +45,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "8+",
     img: "/pippi_poster.jpg",
+    status: "Medium"
   },
   {
     title: "Pippi",
@@ -51,6 +55,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "8+",
     img: "/pippi_poster.jpg",
+    status: "Many"
   },
   {
     title: "Titanic",
@@ -60,6 +65,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/titanic_poster.jpg",
+    status: "Many"
   },
   {
     title: "Sleepers",
@@ -69,6 +75,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/sleepers_poster.jpg",
+    status: "Low"
   },
   {
     title: "Sleepers",
@@ -78,6 +85,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/sleepers_poster.jpg",
+    status: "Many"
   },
   {
     title: "Pippi",
@@ -87,6 +95,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "8+",
     img: "/pippi_poster.jpg",
+    status: "Low"
   },
   {
     title: "Titanic",
@@ -96,6 +105,7 @@ const visningar = [
     sub: "(Sv. text)",
     age: "15+",
     img: "/titanic_poster.jpg",
+    status: "Low"
   },
 ];
 
@@ -104,13 +114,12 @@ const visningar = [
 export default function HomePage() {
   const [salong, setSalong] = useState < number[]|[]>([]);
   const handleChangeSalong = (val: number[] | []) => setSalong(val);
-  const [showMenu, setShowMenu] = useState(true);
-  //const handleShowMenu = () => setShowMenu((showMenu) => !showMenu)
   return (
     <section className="home-section">
       <main className="home_main">
         <h1 className="h1_logo my-4">Filmvisarna</h1>
-        <Carousel interval={null} className="mb-2">
+        <WideNavBar />
+        <Carousel interval={5000} className="mb-2">
           <Carousel.Item>
             <Image src="/sleepers_slide.jpg" alt="Sleepers" fluid />
             <Carousel.Caption className="h3_film_strip top-50 start-50 translate-middle py-3">
@@ -149,68 +158,99 @@ export default function HomePage() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        <Container fluid className="position-relative">
-          {/* <Row>
-          <Col></Col>
-        </Row> */}
-          {/* <Row className="ms-0 me-0">
-          <Col></Col>
-        </Row> */}
-          <Row className="sticky-top bg-body m-auto top-outline">
-            <Row className="mt-3 flex-row justify-content-around bg-body">
-              <Col>
+
+        <Container fluid className="position-relative mt-5 pt-3 pb-5">
+          <Row className="sticky-top bg-body top-outline mb-5" fluid>
+            <Stack direction="vertical" gap={3} className="py-3">
+              <Stack direction="horizontal" gap={3}>
                 <DropdownButton
                   id="Datum"
                   title="Datum"
-                  className="text-secondary"
+                  className="text-secondary mx-auto"
+                  variant="outline-secondary"
                 >
                   <Dropdown.Item as="button">Fredag 20/9</Dropdown.Item>
                   <Dropdown.Item as="button">Lördag 21/9</Dropdown.Item>
                   <Dropdown.Item as="button">Söndag 21/9</Dropdown.Item>
                 </DropdownButton>
-              </Col>
-              <Col>
-                <DropdownButton id="Åldergräns" title="Åldergräns">
+                <DropdownButton
+                  id="Åldergräns"
+                  title="Åldergräns"
+                  className="mx-auto"
+                  variant="outline-secondary"
+                >
                   <Dropdown.Item as="button">Barntillåten</Dropdown.Item>
                   <Dropdown.Item as="button">Från 7 år</Dropdown.Item>
                   <Dropdown.Item as="button">Från 11 år</Dropdown.Item>
                   <Dropdown.Item as="button">Från 15 år</Dropdown.Item>
                 </DropdownButton>
-              </Col>
-            </Row>
-            <Row className="mt-3 mb-3 justify-content-center bg-body">
-              <Col>
-                <ToggleButtonGroup
-                  type="checkbox"
-                  value={salong}
-                  onChange={handleChangeSalong}
-                  className="align-center justify-content-between"
+              </Stack>
+              <ToggleButtonGroup
+                type="checkbox"
+                value={salong}
+                onChange={handleChangeSalong}
+                className="align-center justify-content-between"
+              >
+                <ToggleButton
+                  id="tbg-btn-1"
+                  value={1}
+                  variant="outline-secondary"
                 >
-                  <ToggleButton id="tbg-btn-1" value={1}>
-                    Stora Salongen
-                  </ToggleButton>
-                  <ToggleButton id="tbg-btn-2" value={2}>
-                    Lilla Salongen
-                  </ToggleButton>
-                </ToggleButtonGroup>
-              </Col>
-            </Row>
+                  Stora Salongen
+                </ToggleButton>
+                <ToggleButton
+                  id="tbg-btn-2"
+                  value={2}
+                  variant="outline-secondary"
+                >
+                  Lilla Salongen
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Stack>
           </Row>
-
-          <Row>
+          <Row className="">
             <Col>Kommande visningar</Col>
           </Row>
-          <Row xs={2} sm={3} lg={4}>
+          <Row>
+            <Stack direction="horizontal" gap={3}>
+              <p className="d-flex flex-row align-items-center gap-2">
+                <div className="less-tickets-circle"></div>
+                Färre lediga platser
+              </p>
+              <p className="d-flex flex-row align-items-center gap-2">
+                <div className="sold-out-circle"></div>
+                Utsålt
+              </p>
+            </Stack>
+          </Row>
+          <Row xs={2} sm={3} lg={4} className="row-gap-5 pt-5">
             {visningar.map((visning, visIndex) => (
               <Col key={visIndex}>
-                <Card>
+                <Card
+                  border={
+                    visning.status === "Low"
+                      ? "danger"
+                      : visning.status === "Medium"
+                      ? "warning"
+                      : "secondary"
+                  }
+                >
                   <Card.Img variant="top" src={visning.img} />
                   <Card.Body>
-                    {/* <Card.Title>{visning.title}</Card.Title> */}
-                    <Card.Text>
-                      <p>{visning.date}</p>
-                      <p>{visning.time}</p>
+                    <Card.Text>{visning.date}</Card.Text>
+                    <Card.Text>{visning.time}</Card.Text>
+                    <Card.Text className="fw-lighter">{visning.age}</Card.Text>
+                    <Card.Text className="fw-lighter">
+                      {visning.lang}
+                      {"  "}
+                      {visning.sub}
                     </Card.Text>
+                    <Card.Link>
+                      <Button>Boka</Button>
+                    </Card.Link>
+                    <Card.Link>
+                      <Button>Info</Button>
+                    </Card.Link>
                   </Card.Body>
                 </Card>
               </Col>
@@ -218,51 +258,7 @@ export default function HomePage() {
           </Row>
         </Container>
       </main>
-      <Navbar
-        fixed="bottom"
-        bg="primary"
-        className="navbar-bottom"
-        style={showMenu ? { height: "16vh" } : {}}
-      >
-        <Container className="flex-column">
-          {showMenu ? (
-            <Row xs={3} md={3} lg={3} className="navbar-row">
-              <Col>
-                <Nav.Link href="#login">Logga in</Nav.Link>
-              </Col>
-              <Col>
-                <Nav.Link href="#blimedlem">Bli medlem</Nav.Link>
-              </Col>
-              <Col>
-                <Nav.Link href="#avboka">Avboka platser</Nav.Link>
-              </Col>
-            </Row>
-          ) : null}
-          <Row xs={3} md={3} lg={3} className="navbar-row">
-            <Col>
-              <Nav.Link href="#home">Hem</Nav.Link>
-            </Col>
-            <Col>
-              <Nav.Link href="#filmer">Filmer</Nav.Link>
-            </Col>
-            <Col>
-              <button
-                type="button"
-                onClick={() => setShowMenu((showMenu) => !showMenu)}
-              >
-                Meny
-              </button>
-            </Col>
-          </Row>
-        </Container>
-      </Navbar>
-      {/* <Offcanvas show={showMenu} onHide={() => setShowMenu((showMenu) => !showMenu)} scroll={true} backdrop={true} backdropClassName="customBackdrop" placement="bottom" name="bottom">
-        <Offcanvas.Body>
-          <p>Avboka platser</p>
-          <p>Logga in</p>
-          <p>Bli medlem</p>
-        </Offcanvas.Body>
-      </Offcanvas> */}
+      <MobileNavBar />
     </section>
   );
 }
