1e9c75395276856f22e92d6403d3a8496c687a1d
Staffan Björk <staffanbjork86@gmail.com>
2024-09-24 12:07:39 +0200

    WideNavBar - fixed links and colors and css for logo

--- src/components/WideNavBar/WideNavBar.tsx
+++ src/components/WideNavBar/WideNavBar.tsx
@@ -11,22 +11,35 @@ export default function WideNavBar() {
   const handleLoginClose = () => setShowLoginModal(false);
 
   return (
-    <>
+    <header className="logo-menu-header">
+      <h1 className="h1_logo my-4">Filmvisarna</h1>
       {/* Navbar */}
       <Navbar bg="primary" className="wide-navbar justify-content-around">
-        <Nav.Link href="#hem" className="fw-medium">Hem</Nav.Link>
-        <Nav.Link href="#filmer" className="fw-medium">Filmer</Nav.Link>
-        <Nav.Link href="#visningar" className="fw-medium">Visningar</Nav.Link>
-        <Nav.Link href="#avboka" className="fw-medium">Avboka biljetter</Nav.Link>
+        <Nav.Link href="/" className="fw-medium">
+          Hem
+        </Nav.Link>
+        <Nav.Link href="/filmer" className="fw-medium">
+          Filmer
+        </Nav.Link>
+        <Nav.Link href="/visningar" className="fw-medium">
+          Visningar
+        </Nav.Link>
+        <Nav.Link href="/avboka" className="fw-medium">
+          Avboka biljetter
+        </Nav.Link>
 
         {/* Open Login Modal on Click */}
-        <Nav.Link className="fw-medium" onClick={handleLoginShow}>Logga in</Nav.Link>
+        <Nav.Link className="fw-medium" onClick={handleLoginShow}>
+          Logga in
+        </Nav.Link>
 
-        <Nav.Link href="#blimedlem" className="fw-medium">Bli medlem</Nav.Link>
+        <Nav.Link href="/blimedlem" className="fw-medium">
+          Bli medlem
+        </Nav.Link>
       </Navbar>
 
       {/* Login Modal */}
       <LoginModal show={showLoginModal} handleClose={handleLoginClose} />
-    </>
+    </header>
   );
 }
