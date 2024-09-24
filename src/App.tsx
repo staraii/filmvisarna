f27e1ccaf2df6ac560b3738fcf3ba7adeda72dac
Staffan Björk <staffanbjork86@gmail.com>
2024-09-24 08:55:45 +0200

    MobileNavBar added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Register from './components/Register';
+import MobileNavBar from './components/MobileNavBar/MobileNavBar';
 
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+      <MobileNavBar />
+    </>
   );
 }
 
