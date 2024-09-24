f9d0b18a5118ffc56213f638afb941066e286b78
Staffan Björk <staffanbjork86@gmail.com>
2024-09-24 15:50:56 +0200

    MobileNavBar - updated with new home icon

--- src/components/MobileNavBar/MobileNavBar.tsx
+++ src/components/MobileNavBar/MobileNavBar.tsx
@@ -19,13 +19,13 @@ export default function MobileNavBar() {
       <Nav.Link href="/" className="icon-link">
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          id="Outline"
+          viewBox="0 0 24 24"
           width="32"
           height="32"
           fill="#efecf8"
-          className="bi bi-house"
-          viewBox="0 0 16 16"
         >
-          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+          <path d="M23.121,9.069,15.536,1.483a5.008,5.008,0,0,0-7.072,0L.879,9.069A2.978,2.978,0,0,0,0,11.19v9.817a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V11.19A2.978,2.978,0,0,0,23.121,9.069ZM15,22.007H9V18.073a3,3,0,0,1,6,0Zm7-1a1,1,0,0,1-1,1H17V18.073a5,5,0,0,0-10,0v3.934H3a1,1,0,0,1-1-1V11.19a1.008,1.008,0,0,1,.293-.707L9.878,2.9a3.008,3.008,0,0,1,4.244,0l7.585,7.586A1.008,1.008,0,0,1,22,11.19Z" />
         </svg>
       </Nav.Link>
       <Nav.Link href="/filmer" className="icon-link">
@@ -60,8 +60,8 @@ export default function MobileNavBar() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
+          width="42"
+          height="42"
           fill="#efecf8"
           className="bi bi-three-dots"
           viewBox="0 0 16 16"
