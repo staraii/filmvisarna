e9cfd6919ceeb77754904b907fe71dc3074f8d4e
Konvvit <stackov3rflow@hotmail.com>
2024-09-26 12:57:18 +0200

    Update
    Changed language from eng to swe

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,27 +3,27 @@ import "./footer.css"; // Include any custom styles you want
 export default function Footer() {
   return (
     <footer className="footer">
-      <hr className="footer-separator" /> {/* Line above the footer */}
+      <hr className="footer-separator" /> {/* Linje ovanför foten */}
       <div className="container">
         <div className="row">
           <div className="col-md-4 mb-3">
-            <h5>About Filmvisarna</h5>
+            <h5>Om Filmvisarna</h5>
             <p>
-              Your go-to platform for the latest movies, showtimes, and updates.
+              Din plattform för de senaste filmerna, visningstider och uppdateringar.
             </p>
           </div>
 
           <div className="col-md-4 mb-3">
-            <h5>Contact Us</h5>
+            <h5>Kontakta oss</h5>
             <ul className="list-unstyled">
-              <li>Email: info@filmvisarna.com</li>
-              <li>Phone: +46 123 456 789</li>
-              <li>Address: 123 Movie Street, Stockholm, Sweden</li>
+              <li>E-post: info@filmvisarna.com</li>
+              <li>Telefon: +46 123 456 789</li>
+              <li>Adress: 123 Movie Street, Stockholm, Sverige</li>
             </ul>
           </div>
 
           <div className="col-md-4 mb-3">
-            <h5>Follow Us</h5>
+            <h5>Följ oss</h5>
             <ul className="list-unstyled d-flex">
               <li className="mr-3">
                 <a href="#" className="text-light">Facebook</a>
@@ -37,12 +37,13 @@ export default function Footer() {
             </ul>
           </div>
         </div>
-        <hr className="footer-separator" /> {/* Line separating footer content from copyright */}
+        <hr className="footer-separator" /> {/* Linje som skiljer innehållet från copyright */}
         <div className="text-center py-3">
-          <p>&copy; {new Date().getFullYear()} Filmvisarna. All Rights Reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Filmvisarna. Alla rättigheter förbehållna.</p>
         </div>
       </div>
     </footer>
   );
 }
+
 
