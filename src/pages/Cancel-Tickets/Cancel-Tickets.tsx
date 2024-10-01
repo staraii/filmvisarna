e4a4293252d39183d1b5c238d86a88b49f5602e4
Jens CN <jens.sydaker@gmail.com>
2024-10-01 12:51:55 +0200

    la till lite error checking

--- src/pages/Cancel-Tickets/Cancel-Tickets.tsx
+++ src/pages/Cancel-Tickets/Cancel-Tickets.tsx
@@ -1,13 +1,35 @@
 import "./Cancel-Tickets.css";
-import React, { useState } from "react"; // Importera useState från React
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 function CancelTickets() {
   const [showModal, setShowModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [showErrorModal, setShowErrorModal] = useState(false);
+
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
+
+  const handleShowErrorModal = () => setShowErrorModal(true);
+  const handleCloseErrorModal = () => setShowErrorModal(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const emailPattern =
+      /^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|outlook\.com)$/;
+    if (!emailPattern.test(email)) {
+      handleShowErrorModal();
+    } else {
+      handleShowModal();
+    }
+  };
 
   return (
     <section className="Cancel-Tickets">
@@ -15,25 +37,31 @@ function CancelTickets() {
         <h3>Avboka Biljetter</h3>
       </div>
       <div className="Input-Fields">
-        <Form>
+        <Form onSubmit={handleSubmit}>
+          {" "}
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Mailadress</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={handleEmailChange}
+              className="Custom-Input"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Bokningsnummer</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={3} className="Custom-Input" />
           </Form.Group>
+          <div className="Cancel-Tickets-Button">
+            <Button variant="primary" type="submit">
+              {" "}
+              Avboka
+            </Button>
+          </div>
         </Form>
       </div>
-      <div className="Cancel-Tickets-Button">
-        {/* När knappen klickas, visas modalen */}
-        <Button variant="primary" onClick={handleShowModal}>
-          Avboka
-        </Button>
-      </div>
 
-      {/* Modal-komponent som visas baserat på state */}
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>Biljetten är avbokad</Modal.Title>
@@ -43,6 +71,23 @@ function CancelTickets() {
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
+            Stäng
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showErrorModal} onHide={handleCloseErrorModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Felaktig Mailadress</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Vänligen ange en giltig mailadress som slutar på @gmail.com,
+            @hotmail.com eller @outlook.com.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseErrorModal}>
             Stäng
           </Button>
         </Modal.Footer>
