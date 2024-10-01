268bfe4380fef879e1667c00f870d9cd477e2818
Konvvit <stackov3rflow@hotmail.com>
2024-10-01 15:09:37 +0200

    Update
    Update

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,11 @@ import MovieCalendar from "./components/MovieCalendar/MovieCalendar";
 import CancelTickets from "./pages/Cancel-Tickets/Cancel-Tickets";
 import Movies from "./pages/Movies/Movies";
 import LoginModal from "./components/Login-pop-up/LoginModal"; // Ensure correct import
-
+import LoginPage from "./components/Login-pop-up/LoginMobile";
 
 import "./App.css";
 import PasswordReset from "./components/Login-pop-up/passwordReset";
+
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // State for logged in status
@@ -56,7 +57,7 @@ export default function App() {
             <Route path="/bio-kalender" element={<MovieCalendar />} />
             <Route path="/avboka" element={<CancelTickets />} />
             <Route path="/loggain" element={<LoginPage />} />
-             <Route path="/passwordreset" element={<PasswordReset />} />
+             <Route path="/forgot-password" element={<PasswordReset />} />
             
           </Routes>
         </div>
