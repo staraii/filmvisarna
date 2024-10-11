0b30494315070b4b82fdd878f60dd412b025ea2a
Jens CN <jens.sydaker@gmail.com>
2024-10-11 11:56:22 +0200

    fixade så att man kan byta veckor på movie calendar

--- frontend/src/components/MovieCalendar/MovieCalendar.tsx
+++ frontend/src/components/MovieCalendar/MovieCalendar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./MovieCalendar.css";
 import Carousel from "react-bootstrap/Carousel";
 import Accordion from "react-bootstrap/Accordion";
@@ -8,6 +9,367 @@ import Sleepers from "../../../public/sleepers_poster.jpg";
 import Titanic from "../../../public/titanic_poster.jpg";
 
 function MovieCalendar() {
+  const [activeWeekIndex, setActiveWeekIndex] = useState(0); // State för att hålla koll på aktiv vecka
+
+  const weeks = [
+    { title: "Vecka 40", startDate: "2024-10-01" },
+    { title: "Vecka 41", startDate: "2024-10-08" },
+    { title: "Vecka 42", startDate: "2024-10-15" },
+    { title: "Vecka 43", startDate: "2024-10-22" },
+  ];
+
+  // Filmer för varje dag i varje vecka
+  const daysData = [
+    // Vecka 40
+    [
+      {
+        day: "Måndag - September 30",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Tisdag - Oktober 1",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Onsdag - Oktober 2",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Torsdag - Oktober 3",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Fredag - Oktober 4",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Lördag - Oktober 5",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Söndag - Oktober 6",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+    ],
+    // Vecka 41
+    [
+      {
+        day: "Måndag - Oktober 7",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Tisdag - Oktober 8",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Onsdag - Oktober 9",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Torsdag - Oktober 10",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Fredag - Oktober 11",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Lördag - Oktober 12",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Söndag - Oktober 13",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+    ],
+    // Vecka 42
+    [
+      {
+        day: "Måndag - Oktober 14",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Tisdag - Oktober 15",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Onsdag - Oktober 16",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Torsdag - Oktober 17",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Fredag - Oktober 18",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Lördag - Oktober 19",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Söndag - Oktober 20",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+    ],
+    // Vecka 43
+    [
+      {
+        day: "Måndag - Oktober 21",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Tisdag - Oktober 22",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Onsdag - Oktober 23",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Torsdag - Oktober 24",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+      {
+        day: "Fredag - Oktober 25",
+        movies: [
+          {
+            title: "Pippi Långstrump (1970)",
+            genre: "Barnlitteratur",
+            age: "8+",
+            time: "17:00",
+            poster: Pippi,
+          },
+        ],
+      },
+      {
+        day: "Lördag - Oktober 26",
+        movies: [
+          {
+            title: "Titanic (1997)",
+            genre: "Romantik, Drama",
+            age: "15+",
+            time: "19:00",
+            poster: Titanic,
+          },
+        ],
+      },
+      {
+        day: "Söndag - Oktober 27",
+        movies: [
+          {
+            title: "Sleepers (1996)",
+            genre: "Drama, Thriller",
+            age: "15+",
+            time: "22:00",
+            poster: Sleepers,
+          },
+        ],
+      },
+    ],
+  ];
+
   return (
     <section className="Movie-Calendar">
       <div className="Movie-Text">
@@ -15,119 +377,61 @@ function MovieCalendar() {
         <p>Se vilka filmer som går de kommande veckorna.</p>
       </div>
       <div className="Weeks">
-        <Carousel slide={false} indicators={false} interval={null} wrap={false}>
-          {["Vecka 40", "Vecka 41", "Vecka 42", "Vecka 43"].map(
-            (week, index) => (
-              <Carousel.Item key={index}>
-                <div className="carousel-text">
-                  <h3>{week}</h3>
-                </div>
-              </Carousel.Item>
-            )
-          )}
+        <Carousel
+          slide={false}
+          indicators={false}
+          interval={null}
+          wrap={false}
+          activeIndex={activeWeekIndex}
+          onSelect={(selectedIndex) => setActiveWeekIndex(selectedIndex)}
+        >
+          {weeks.map((week, index) => (
+            <Carousel.Item key={index}>
+              <div className="carousel-text">
+                <h3>{week.title}</h3>
+              </div>
+            </Carousel.Item>
+          ))}
         </Carousel>
       </div>
       <div className="Days">
         <Accordion>
-          {[
-            "Måndag",
-            "Tisdag",
-            "Onsdag",
-            "Torsdag",
-            "Fredag",
-            "Lördag",
-            "Söndag",
-          ].map((day, index) => (
-            <Accordion.Item eventKey={index.toString()} key={index}>
-              <Accordion.Header>{day}</Accordion.Header>
+          {daysData[activeWeekIndex].map((dayData, dayIndex) => (
+            <Accordion.Item eventKey={dayIndex.toString()} key={dayIndex}>
+              <Accordion.Header>{dayData.day}</Accordion.Header>
               <Accordion.Body>
                 <div className="Poster-Container">
-                  <div className="Poster-Description">
-                    <Card style={{ width: "16rem" }}>
-                      <Card.Img variant="top" src={Pippi} />
-                      <Card.Body>
-                        <Card.Title>Pippi Långstrump (1970)</Card.Title>
-                        <div className="Genre-Age">
-                          <Card.Text className="Genre">
-                            Barnlitteratur
-                          </Card.Text>
-                          <Card.Text className="Age Age-8">
-                            <p>8+</p>
-                          </Card.Text>
-                        </div>
+                  {dayData.movies.map((movie, index) => (
+                    <div className="Poster-Description" key={index}>
+                      <Card style={{ width: "16rem" }}>
+                        <Card.Img variant="top" src={movie.poster} />
+                        <Card.Body>
+                          <Card.Title>{movie.title}</Card.Title>
+                          <div className="Genre-Age">
+                            <Card.Text className="Genre">
+                              {movie.genre}
+                            </Card.Text>
+                            <Card.Text className="Age Age-8">
+                              <p>{movie.age}</p>
+                            </Card.Text>
+                          </div>
 
-                        <div className="Time-Button-Container">
-                          <Card.Text className="Movie-Time">
-                            <p>17:00</p>
-                          </Card.Text>
-                          <Button
-                            className="Movie-Button"
-                            variant="outline-primary"
-                            href="/boka"
-                          >
-                            Boka nu
-                          </Button>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  </div>
-                  <div className="Poster-Description">
-                    <Card style={{ width: "16rem" }}>
-                      <Card.Img variant="top" src={Titanic} />
-                      <Card.Body>
-                        <Card.Title>Titanic (1997)</Card.Title>
-                        <div className="Genre-Age">
-                          <Card.Text className="Genre">
-                            Romantik, Drama
-                          </Card.Text>
-                          <Card.Text className="Age Age-15">
-                            <p>15+</p>
-                          </Card.Text>
-                        </div>
-                        <div className="Time-Button-Container">
-                          <Card.Text className="Movie-Time">
-                            <p>19:00</p>
-                          </Card.Text>
-                          <Button
-                            className="Movie-Button"
-                            variant="outline-primary"
-                            href="/boka"
-                          >
-                            Boka nu
-                          </Button>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  </div>
-                  <div className="Poster-Description">
-                    <Card style={{ width: "16rem" }}>
-                      <Card.Img variant="top" src={Sleepers} />
-                      <Card.Body>
-                        <Card.Title>Sleepers (1996)</Card.Title>
-                        <div className="Genre-Age">
-                          <Card.Text className="Genre">
-                            Drama, Thriller
-                          </Card.Text>
-                          <Card.Text className="Age Age-15">
-                            <p>15+</p>
-                          </Card.Text>
-                        </div>
-
-                        <div className="Time-Button-Container">
-                          <Card.Text className="Movie-Time">
-                            <p>22:00</p>
-                          </Card.Text>
-                          <Button
-                            className="Movie-Button"
-                            variant="outline-primary"
-                            href="/boka"
-                          >
-                            Boka nu
-                          </Button>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  </div>
+                          <div className="Time-Button-Container">
+                            <Card.Text className="Movie-Time">
+                              <p>{movie.time}</p>
+                            </Card.Text>
+                            <Button
+                              className="Movie-Button"
+                              variant="outline-primary"
+                              href="/boka"
+                            >
+                              Boka nu
+                            </Button>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </div>
+                  ))}
                 </div>
               </Accordion.Body>
             </Accordion.Item>
