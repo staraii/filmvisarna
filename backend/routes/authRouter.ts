005e79c3abf6f5623b453130c5c6c371e2a8bba7
Konvvit <stackov3rflow@hotmail.com>
2024-10-15 11:03:02 +0200

    Update
    Added authRouter

--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,8 +1,21 @@
-import express from "express";
-import authController from "../controller/authController.js";
+// Route file for authentications
 
-const router = express.Router();
 
-router.post("/auth", authController.loginUser);
+import { Router } from 'express';
+import { login, logout, getLoggedInUser,register } from '../controller/authController.js';
+
+const router = Router();
+
+// POST: Register
+router.post('/api/register', register);
+
+// POST: Login
+router.post('/api/login', login);
+
+// GET: Check if user is logged in
+router.get('/api/login', getLoggedInUser);
+
+// DELETE: Logout
+router.delete('/api/login', logout);
 
 export default router;
