a69ade465fef986a69b8917cb37dfff2fffe2dff
Konvvit <stackov3rflow@hotmail.com>
2024-10-16 14:03:19 +0200

    Update

--- backend/types/express-session.d.ts
+++ backend/types/express-session.d.ts
@@ -1,10 +1,8 @@
-// backend/@types/express-session.d.ts
-
 import 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
-    userId: number; // Or whatever type your userId is
-    userRole: string; // Add the userRole property
+    userId: number; // or string, depending on your user ID type
+    userRole?: string; // Optional if you're storing roles
   }
 }
