1137aa1edd6d36ea1d875b6489d83fcef09a6999
Staffan Björk <staffanbjork86@gmail.com>
2024-10-29 14:39:28 +0100

    HomePage broken down in components

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -4,85 +4,66 @@ import ScreeningCard from "./ScreeningCard/ScreeningCard";
 import MovieCarousel from "./MovieCarousel/MovieCarousel";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
-import ToggleButton from "react-bootstrap/ToggleButton";
-import Stack from "react-bootstrap/Stack";
 import Footer from "../../components/Footer/Footer";
 import { useLoaderData } from "react-router-dom";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { DualQueryParams, loaderQuery, HomePageMovies, HomePageScreenings } from "../../utils/queryService";
+import { useSuspenseQuery, useQuery } from "@tanstack/react-query";
+import { DualQueryParams, loaderQuery, HomePageMovies, HomePageScreenings, getQueryData } from "../../utils/queryService";
+import ScreeningFilters from "./ScreeningFilters/ScreeningFilters";
 
 
 export default function HomePage() {
+  // Get query parameters from route loader
   const { queryParamsOne, queryParamsTwo } = useLoaderData() as DualQueryParams;
+  
+  // State for handling query filters and parsing query url
+  const [filters, setFilters] = useState({ date: "ALL", age: "ALL", theatre: [], urlPrefix: queryParamsTwo.query, filteredQuery: queryParamsTwo.query });
+  
+  // moviesData prefetched before route is rendered
   const { data: moviesData } = useSuspenseQuery(loaderQuery(queryParamsOne));
-  const { data: screeningsData } = useSuspenseQuery(loaderQuery(queryParamsTwo));
+
+  // screeningsData fetched after component is rendered, depending on filteredQuery string
+  const { data: screeningsData, refetch, isPending, error } = useQuery({ queryKey: [queryParamsTwo.queryName, filters.filteredQuery], queryFn: () => getQueryData(filters.filteredQuery), enabled: !!filters.filteredQuery });
+
+  // Arrays of data to use
   const movies: HomePageMovies[] = moviesData;
   const screenings: HomePageScreenings[] = screeningsData;
-  const [salong, setSalong] = useState<number[]>([]);
-  const handleChangeSalong = (val: number[]) => setSalong(val);
 
+  // Setting filter options, reparsing query url and refetching data
+  const handleFilterChange = (type: string, value: string | number[]) => {
+    let newFilters = { ...filters, [`${type}`]: value };
+
+    const getParsedFilterQuery = () => {
+      const filtersList = [];
+      let filterQuery = filters.urlPrefix;
+      if (newFilters.date === "ALL" && newFilters.age === "ALL" && newFilters.theatre.length === 0) {
+        return filterQuery;
+      }
+      if (newFilters.date !== "ALL") {
+        filtersList.push(`date=${newFilters.date}`)
+      }
+      if (newFilters.age !== "ALL") {
+        filtersList.push(`ageRating<=${newFilters.age}`);
+      }
+      if (newFilters.theatre.length === 1) {
+        filtersList.push(`theatreName=${newFilters.theatre[0] === 1 ? "Stora+Salongen" : "Lilla+Salongen"}`);
+      }
+      if (filtersList.length > 0) {
+        filterQuery += "?" + `${filtersList.length > 1 ? filtersList.join("&") : filtersList[0]}`;
+      }
+      return filterQuery
+    }
+    const newFilterQuery = getParsedFilterQuery();
+    newFilters = {...newFilters, filteredQuery: newFilterQuery}
+    setFilters(newFilters);
+    refetch();
+  }
   return (
     <section className="home-section pb-xs-5">
       <main className="home_main">
         <MovieCarousel movies={movies} />
-
-        {/* Main Content Area */}
         <Container fluid className="position-relative mt-5 pt-3 pb-5">
           {/* Filters Section */}
-          <Row className="sticky-top bg-body top-outline mb-5">
-            <Stack direction="vertical" gap={3} className="py-3">
-              <Stack direction="horizontal" gap={3}>
-                <DropdownButton
-                  id="Datum"
-                  title="Datum"
-                  className="text-secondary mx-auto"
-                  variant="outline-secondary"
-                >
-                  <Dropdown.Item as="button">Fredag 20/9</Dropdown.Item>
-                  <Dropdown.Item as="button">Lördag 21/9</Dropdown.Item>
-                  <Dropdown.Item as="button">Söndag 22/9</Dropdown.Item>
-                </DropdownButton>
-                <DropdownButton
-                  id="Åldergräns"
-                  title="Åldergräns"
-                  className="mx-auto"
-                  variant="outline-secondary"
-                >
-                  <Dropdown.Item as="button">Barntillåten</Dropdown.Item>
-                  <Dropdown.Item as="button">Från 7 år</Dropdown.Item>
-                  <Dropdown.Item as="button">Från 11 år</Dropdown.Item>
-                  <Dropdown.Item as="button">Från 15 år</Dropdown.Item>
-                </DropdownButton>
-              </Stack>
-
-              {/* ToggleButtonGroup for Theater Selection */}
-              <ToggleButtonGroup
-                type="checkbox"
-                value={salong}
-                onChange={handleChangeSalong}
-                className="align-center justify-content-between"
-              >
-                <ToggleButton
-                  id="tbg-btn-1"
-                  value={1}
-                  variant="outline-secondary"
-                >
-                  Stora Salongen
-                </ToggleButton>
-                <ToggleButton
-                  id="tbg-btn-2"
-                  value={2}
-                  variant="outline-secondary"
-                >
-                  Lilla Salongen
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </Stack>
-          </Row>
-
+          <ScreeningFilters filters={filters} handleFilterChange={handleFilterChange} />
           {/* Upcoming Screenings Section */}
           <Row className="mb-5">
             <h3>Kommande visningar</h3>
@@ -96,11 +77,14 @@ export default function HomePage() {
             xl={3}
             xxl={3}
           >
-            {screenings.map((screening) => (<ScreeningCard key={screening.screeningId} {...screening} />))}
+            {isPending && (<p>Hämtar visningar...</p>)}
+            {error && !screenings && (<p>Kunde inte hitta några visningar</p>)}
+            {screenings && screenings.map((screening) => (
+              <ScreeningCard key={screening.screeningId} {...screening} />
+            ))}
           </Row>
         </Container>
       </main>
-
       <Footer />
     </section>
   );
