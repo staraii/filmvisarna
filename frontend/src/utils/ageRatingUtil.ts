efc77578df0201592ace51c6f000b59cc5be8b0d
Staffan Björk <staffanbjork86@gmail.com>
2024-10-30 09:13:51 +0100

    ageRatingFilterUtil function added

--- frontend/src/utils/ageRatingUtil.ts
+++ frontend/src/utils/ageRatingUtil.ts
@@ -1,4 +1,4 @@
-export default function ageRatingUtil(age: string) {
+export function ageRatingUtil(age: string) {
   if (!/^(?:1|7|11|15)$/.test(age)) {
     return;
   }
@@ -6,4 +6,14 @@ export default function ageRatingUtil(age: string) {
   if (age === "7") { return "7+" }
   if (age === "11") { return "11+" }
   if(age === "15"){ return "15+"}
+}
+
+export function ageRatingFilterUtil(age: string) {
+  if (!/^(?:1|7|11|15)$/.test(age)) {
+    return;
+  }
+  if (age === "1") { return "Barntillåten" }
+  if (age === "7") { return "Från 7 år" }
+  if (age === "11") { return "Från 11 år" }
+  if(age === "15") { return "Från 15 år"}
 }
