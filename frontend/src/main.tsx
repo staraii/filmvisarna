0b8a3bd4ddef81c3cd1b8fb3e98743dd55db8cfa
Svintoo <S_maskuniitty@hotmail.se>
2024-11-13 12:53:40 +0100

    again fixed formatting...

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
   QueryClient, 
-  QueryClientProvider } 
-  from "@tanstack/react-query";
+  QueryClientProvider, 
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "../sass/main.scss";
 import App from "./App.tsx";
