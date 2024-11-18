fff10e02d93389d25ea7c883df053f4740bfb66f
Staffan Björk <staffanbjork86@gmail.com>
2024-11-18 10:10:31 +0100

    mailService - qr code added to mail, fixed time and date

--- backend/services/mailService.ts
+++ backend/services/mailService.ts
@@ -21,15 +21,24 @@ export default class MailService {
     console.log(data);
     const bookingData = data[0];
 
-    const img = await QRCode.toDataURL(bookingNumber);
+    const date = new Date(bookingData.screeningTime);
+    const qrCodeDataURL = await QRCode.toDataURL(bookingNumber);
+    
 
     const info = await this.transporter.sendMail({
       from: '"Filmvisarna" <Filmvisarna@hotmail.com>', // sender addresss
       to: "dejuan.watsica63@ethereal.email", // list of receivers
       subject: "Filmvisarna bokning", // Subject line
       text: `Bokningsid:${bookingNumber} `, // plain text body
-      attachDataUrls: true,
-      html: `<b><h2>Tack för din bokning!</h2> <p>Bokningsnummer: ${bookingNumber}</p><p>Film: ${bookingData.movieTitle}</p><p>Datum: ${bookingData.screeningTime}</p><p>Seats: ${bookingData.seats}</p> </b></br><img src="${img}">`, // html body
+      attachments: [
+        {
+          filename: "qrcode.png",
+          content: qrCodeDataURL.split("base64,")[1],
+          encoding: "base64",
+          cid: "qrcode",
+        },
+      ],
+      html: `<b><h2>Tack för din bokning!</h2> <p>Bokningsnummer: ${bookingNumber}</p><p>Film: ${bookingData.movieTitle}</p><p>Datum: ${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}</p><p>Tid: ${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}</p><p>Platsnummer: ${bookingData.seats}</p> </b><img src='cid:qrcode'/>`, // html body
     });
     return "Message sent:%s" + info.messageId;
   }
