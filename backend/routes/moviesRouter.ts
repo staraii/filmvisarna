3aecc4cdf43f84caa530d1f6ceb9d126b8322fcf
Staffan Björk <staffanbjork86@gmail.com>
2024-11-23 13:45:14 +0100

    moviesRouter - updated

--- backend/routes/moviesRouter.ts
+++ backend/routes/moviesRouter.ts
@@ -1,9 +1,9 @@
 import express from "express";
-//import moviesController from "../controller/moviesController.js";
+import moviesController from "../controller/moviesController.js";
 
 const router = express.Router();
 
-//router.get("/movies", moviesController.getMovies);
+router.post("/api/movies", moviesController.postNewMovie);
 
 
 export default router;
